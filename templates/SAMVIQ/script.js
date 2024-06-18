c3a84f780b310dd1c69e24cbad56e511b2fbf748
M-B-Hussain <93214491+M-B-Hussain@users.noreply.github.com>
2024-06-18 22:50:42 +0200

    Delete templates/SAMVIQ directory

--- templates/SAMVIQ/script.js
+++
@@ -1,5 +0,0 @@
-const scoringSlider = document.getElementById('scoring-slider');
-const scoreDisplay = document.getElementById('score-display');
-const playButton = document.getElementById('play-button');
-const stopButton = document.getElementById('stop-button');
-const previousSceneButton = document
